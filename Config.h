f828feb80016ac2ab6842f357e771b1dd173a5c7
ffrancisrramos <ffrancisrramos@gmail.com>
2026-05-12 22:58:57 -0700

    Update Config.h

--- Config.h
+++ Config.h
@@ -17,7 +17,7 @@ vector<PlacementChange> placeWord(vector<string>& grid, string word, int row, in
 
 void removeWord(vector<string>& grid, vector<PlacementChange>& changedCells);
 
-bool solve(vector<string>& grid, vector<string>& words, int index);
+bool generate(vector<string>& grid, vector<string>& words, int index);
 
 //void printGrid(vector<string>& grid);
 
